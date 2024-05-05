0a4e06f30f4992dc556350ba391eae362750d9e0
KeerthiPatil <167600409+KeerthiPatil@users.noreply.github.com>
2024-05-05 17:03:28 -0400

    Update and rename iiitb_rv32i_tb.v to ins_RV32EC_tb.v

--- ins_RV32EC_tb.v
+++ ins_RV32EC_tb.v
@@ -1,9 +1,9 @@
-module iiitb_rv32i_tb;
+module ins_RV32EC_tb;
 
 reg clk,RN;
 wire [31:0]WB_OUT,NPC;
 
-iiitb_rv32i rv32(clk,RN,NPC,WB_OUT);
+ins_RV32EC RV32(clk,RN,NPC,WB_OUT);
 
 
 always #3 clk=!clk;
@@ -12,8 +12,8 @@ initial begin
 RN  = 1'b1;
 clk = 1'b1;
 
-$dumpfile ("iiitb_rv32i.vcd"); //by default vcd
-$dumpvars (0, iiitb_rv32i_tb);
+$dumpfile ("ins_RV32EC.vcd"); //by default vcd
+$dumpvars (0, ins_RV32EC_tb);
   
   #5 RN = 1'b0;
   
